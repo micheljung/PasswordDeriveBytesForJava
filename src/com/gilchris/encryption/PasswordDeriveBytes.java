97fff044f921a6a2b64ce3948278b6adc907e18f
Michel Jung <michel.jung89@gmail.com>
2019-04-02 13:23:25 +0200

    Java-ize PasswordDeriveBytes
    - Fix variable and method names
    
    - Fix wrong `NullPointerException("HashName")` when `iterationCount` is < 1
    
    - Use `Objects.requireNonNull`

--- src/com/gilchris/encryption/PasswordDeriveBytes.java
+++ src/com/gilchris/encryption/PasswordDeriveBytes.java
@@ -1,34 +1,35 @@
-/**
+/*
  * The MIT License (MIT)
  * Copyright (c) 2014, 2016 Changgun Lee <lazysense@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
- * and associated documentation files (the "Software"), to deal in the Software without restriction, 
- * including without limitation the rights to use, copy, modify, merge, publish, distribute, 
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or 
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
- * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package com.gilchris.encryption;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.DigestException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 
 public class PasswordDeriveBytes {
 
-   private String HashNameValue;
-   private byte[] SaltValue;
-   private int IterationsValue;
+   private String hashNameValue;
+   private byte[] saltValue;
+   private int iterationsValue;
 
    private MessageDigest hash;
    private int state;
@@ -37,193 +38,186 @@ public class PasswordDeriveBytes {
    private byte[] output;
    private byte[] firstBaseOutput;
    private int position;
-   private int hashnumber;
+   private int hashNumber;
    private int skip;
 
    public PasswordDeriveBytes(String strPassword, byte[] rgbSalt) {
-       Prepare(strPassword, rgbSalt, "SHA-1", 100);
+      prepare(strPassword, rgbSalt, "SHA-1", 100);
    }
 
    public PasswordDeriveBytes(String strPassword, byte[] rgbSalt, String strHashName, int iterations) {
-       Prepare(strPassword, rgbSalt, strHashName, iterations);
+      prepare(strPassword, rgbSalt, strHashName, iterations);
    }
 
    public PasswordDeriveBytes(byte[] password, byte[] salt) {
-       Prepare(password, salt, "SHA-1", 100);
+      prepare(password, salt, "SHA-1", 100);
    }
 
 
    public PasswordDeriveBytes(byte[] password, byte[] salt, String hashName, int iterations) {
-       Prepare(password, salt, hashName, iterations);
+      prepare(password, salt, hashName, iterations);
    }
 
-   private void Prepare(String strPassword, byte[] rgbSalt, String strHashName, int iterations) {
-       if (strPassword == null)
-           throw new NullPointerException("strPassword");
+   private void prepare(String strPassword, byte[] rgbSalt, String strHashName, int iterations) {
+      Objects.requireNonNull(strPassword, "Password must not be null");
 
-       byte[] pwd = null;
-       try {
-           pwd = strPassword.getBytes("ASCII");
-       } catch (UnsupportedEncodingException e) {
-           e.printStackTrace();
-       }
-       Prepare(pwd, rgbSalt, strHashName, iterations);
+      byte[] pwd = strPassword.getBytes(StandardCharsets.US_ASCII);
+      prepare(pwd, rgbSalt, strHashName, iterations);
    }
 
-   private void Prepare(byte[] password, byte[] rgbSalt, String strHashName, int iterations) {
-       if (password == null)
-           throw new NullPointerException("password");
+   private void prepare(byte[] password, byte[] rgbSalt, String strHashName, int iterations) {
+      Objects.requireNonNull(password, "Password must not be null");
 
-       this.password = password;
+      this.password = password;
 
-       state = 0;
-       setSalt(rgbSalt);
-       setHashName(strHashName);
-       setIterationCount(iterations);
+      state = 0;
+      setSalt(rgbSalt);
+      setHashName(strHashName);
+      setIterationCount(iterations);
 
-       initial = new byte[hash.getDigestLength()];
+      initial = new byte[hash.getDigestLength()];
    }
 
    public byte[] getSalt() {
-       if (SaltValue == null)
-           return null;
-       return SaltValue;
+      if (saltValue == null) {
+         return null;
+      }
+      return saltValue;
    }
 
    public void setSalt(byte[] salt) {
-       if (state != 0) {
-           throw new SecurityException("Can't change this property at this stage");
-       }
-       if (salt != null)
-           SaltValue = salt;
-       else
-           SaltValue = null;
+      if (state != 0) {
+         throw new SecurityException("Can't change this property at this stage");
+      }
+      if (salt != null) {
+         saltValue = salt;
+      } else {
+         saltValue = null;
+      }
    }
 
    public String getHashName() {
-       return HashNameValue;
+      return hashNameValue;
    }
 
    public void setHashName(String hashName) {
-       if (hashName == null)
-           throw new NullPointerException("HashName");
-       if (state != 0) {
-           throw new SecurityException("Can't change this property at this stage");
-       }
-       HashNameValue = hashName;
+      Objects.requireNonNull(hashName, "Hash name must not be null");
+      if (state != 0) {
+         throw new SecurityException("Can't change this property at this stage");
+      }
+      hashNameValue = hashName;
 
-       try {
-           hash = MessageDigest.getInstance(hashName);
-       } catch (NoSuchAlgorithmException e) {
-           e.printStackTrace();
-       }
+      try {
+         hash = MessageDigest.getInstance(hashName);
+      } catch (NoSuchAlgorithmException e) {
+         e.printStackTrace();
+      }
    }
 
    public int getIterationCount() {
-       return IterationsValue;
+      return iterationsValue;
    }
 
    public void setIterationCount(int iterationCount) {
-       if (iterationCount < 1)
-           throw new NullPointerException("HashName");
-       if (state != 0) {
-           throw new SecurityException("Can't change this property at this stage");
-       }
-       IterationsValue = iterationCount;
+      if (iterationCount < 1) {
+         throw new IllegalArgumentException("Iteration count must be greater than 0");
+      }
+      if (state != 0) {
+         throw new SecurityException("Can't change this property at this stage");
+      }
+      iterationsValue = iterationCount;
    }
 
-   public byte[] GetBytes(int cb) throws DigestException {
-       if (cb < 1) {
-           throw new IndexOutOfBoundsException("cb");
-       }
+   public byte[] getBytes(int cb) throws DigestException {
+      if (cb < 1) {
+         throw new IndexOutOfBoundsException("cb");
+      }
 
-       if (state == 0) {
-           Reset();
-           state = 1;
-       }
+      if (state == 0) {
+         reset();
+         state = 1;
+      }
 
-       byte[] result = new byte[cb];
-       int cpos = 0;
-       // the initial hash (in reset) + at least one iteration
-       int iter = Math.max(1, IterationsValue - 1);
+      byte[] result = new byte[cb];
+      int cpos = 0;
+      // the initial hash (in reset) + at least one iteration
+      int iter = Math.max(1, iterationsValue - 1);
 
-       // start with the PKCS5 key
-       if (output == null) {
-           // calculate the PKCS5 key
-           output = initial;
+      // start with the PKCS5 key
+      if (output == null) {
+         // calculate the PKCS5 key
+         output = initial;
 
-           // generate new key material
-           for (int i = 0; i < iter - 1; i++) {
-               output = hash.digest(output);
-           }
-       }
+         // generate new key material
+         for (int i = 0; i < iter - 1; i++) {
+            output = hash.digest(output);
+         }
+      }
 
-       while (cpos < cb) {
-           byte[] output2 = null;
-           if (hashnumber == 0) {
-               // last iteration on output
-               output2 = hash.digest(output);
-           } else if (hashnumber < 1000) {
-               byte[] n = Integer.toString(hashnumber).getBytes();
-               output2 = new byte[output.length + n.length];
-               for (int j = 0; j < n.length; j++) {
-                   output2[j] = n[j];
-               }
-               System.arraycopy(output, 0, output2, n.length, output.length);
-               // don't update output
-               output2 = hash.digest(output2);
-           } else {
-               throw new SecurityException("too long");
-           }
+      while (cpos < cb) {
+         byte[] output2;
+         if (hashNumber == 0) {
+            // last iteration on output
+            output2 = hash.digest(output);
+         } else if (hashNumber < 1000) {
+            byte[] n = Integer.toString(hashNumber).getBytes();
+            output2 = new byte[output.length + n.length];
+            System.arraycopy(n, 0, output2, 0, n.length);
+            System.arraycopy(output, 0, output2, n.length, output.length);
+            // don't update output
+            output2 = hash.digest(output2);
+         } else {
+            throw new SecurityException("too long");
+         }
 
-           int rem = output2.length - position;
-           int l = Math.min(cb - cpos, rem);
-           System.arraycopy(output2, position, result, cpos, l);
+         int rem = output2.length - position;
+         int l = Math.min(cb - cpos, rem);
+         System.arraycopy(output2, position, result, cpos, l);
 
-           cpos += l;
-           position += l;
-           while (position >= output2.length) {
-               position -= output2.length;
-               hashnumber++;
-           }
-       }
+         cpos += l;
+         position += l;
+         while (position >= output2.length) {
+            position -= output2.length;
+            hashNumber++;
+         }
+      }
 
-       // saving first output length
-       if (state == 1) {
-           if (cb > 20) {
-               skip = 40 - result.length;
-           } else {
-               skip = 20 - result.length;
-           }
-           firstBaseOutput = new byte[result.length];
-           System.arraycopy(result, 0, firstBaseOutput, 0, result.length);
-           state = 2;
-       }
-       // processing second output
-       else if (skip > 0) {
-           byte[] secondBaseOutput = new byte[(firstBaseOutput.length + result.length)];
-           System.arraycopy(firstBaseOutput, 0, secondBaseOutput, 0, firstBaseOutput.length);
-           System.arraycopy(result, 0, secondBaseOutput, firstBaseOutput.length, result.length);
-           System.arraycopy(secondBaseOutput, skip, result, 0, skip);
+      // saving first output length
+      if (state == 1) {
+         if (cb > 20) {
+            skip = 40 - result.length;
+         } else {
+            skip = 20 - result.length;
+         }
+         firstBaseOutput = new byte[result.length];
+         System.arraycopy(result, 0, firstBaseOutput, 0, result.length);
+         state = 2;
+      }
+      // processing second output
+      else if (skip > 0) {
+         byte[] secondBaseOutput = new byte[(firstBaseOutput.length + result.length)];
+         System.arraycopy(firstBaseOutput, 0, secondBaseOutput, 0, firstBaseOutput.length);
+         System.arraycopy(result, 0, secondBaseOutput, firstBaseOutput.length, result.length);
+         System.arraycopy(secondBaseOutput, skip, result, 0, skip);
 
-           skip = 0;
-       }
+         skip = 0;
+      }
 
-       return result;
+      return result;
    }
 
-   public void Reset() throws DigestException {
-       state = 0;
-       position = 0;
-       hashnumber = 0;
-       skip = 0;
+   public void reset() throws DigestException {
+      state = 0;
+      position = 0;
+      hashNumber = 0;
+      skip = 0;
 
-       if (SaltValue != null) {
-           hash.update(password, 0, password.length);
-           hash.update(SaltValue, 0, SaltValue.length);
-           hash.digest(initial, 0, initial.length);
-       } else {
-           initial = hash.digest(password);
-       }
+      if (saltValue != null) {
+         hash.update(password, 0, password.length);
+         hash.update(saltValue, 0, saltValue.length);
+         hash.digest(initial, 0, initial.length);
+      } else {
+         initial = hash.digest(password);
+      }
    }
 }
